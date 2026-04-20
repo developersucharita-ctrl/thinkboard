d97120d37de5b1c2a27779df26fc44b921259c4c
developersucharita-ctrl <developersucharita@gmail.com>
2026-04-20 18:38:03 +0530

    Prepared for deployment - 2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './app.css'
+import './App.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
