156c942d7d89d7c3cba9f025ff20503d73ea969a
developersucharita-ctrl <developersucharita@gmail.com>
2026-04-20 18:42:22 +0530

    Prepared for deployment - 3

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import RateLimitedUi from '../components/RateLimitedUi'
 import api from "../lib/axios.js"
 import toast from "react-hot-toast"
